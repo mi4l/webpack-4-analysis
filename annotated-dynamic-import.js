9a9962a9b4c10e8e354631a8bbc0a31e85223986
Mike Skalandunas <mskalandunas@gmail.com>
2020-08-23 16:50:19 -0700

    finish annotating dynamic import

---
+++ annotated-dynamic-import.js
@@ -0,0 +1,33 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],{
+    /*
+        - IIFE
+        - Pushes a data structure similar in shape to the original `modules` argument
+          into window.webpackJsonp
+        - The [0] represents the chunk name (0.js)
+
+    /***/ "./src/todo.js":
+    /*!*********************!*\
+      !*** ./src/todo.js ***!
+      \*********************/
+    /*! exports provided: default */
+    /***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+    "use strict";
+    eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\n  complete: el => el.style = 'text-decoration: line-through;'\n});\n\n//# sourceURL=webpack:///./src/todo.js?");
+    /*
+      Code here is executed similarly to the other files. The only major difference is that this one
+      has a default export. This is simply set to a `default` prop on the `__webpack_exports__` object,
+      further mutating it. In the end it'll look something like:
+
+      {
+        default: {
+          complete: f,
+          Symbol(Symbol.toStringTag): 'Module',
+          __esModule: true,
+          proto: Object
+        }
+      }
+    */
+    /***/ })
+
+    }]);
