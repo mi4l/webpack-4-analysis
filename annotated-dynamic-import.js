deb991967d72245d984f954bf148fea259d2f8ef
Mike Skalandunas <mskalandunas@gmail.com>
2020-08-30 17:36:30 -0700

    format annotated-dynamic-import.js

--- annotated-dynamic-import.js
+++ annotated-dynamic-import.js
@@ -1,9 +1,57 @@
 (window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],{
     /*
-        - IIFE
-        - Pushes a data structure similar in shape to the original `modules` argument
-          into window.webpackJsonp
-        - The [0] represents the chunk name (0.js)
+        Runtime:
+        1. window['webpackJsonp'] || [] expression is evaluated and
+           a prototypical array or a modified `webpackJsonp` array is
+           returned. If it is the `webpackJsonp` array is returned,
+           its push method has been overwritten to be webpack's
+           `webpackJsonpCallback`.
+        2. result.push is called with the value of:
+            [
+                [0],
+                {
+                    './src/todo.js': (function(module, __webpack_exports__, __webpack_require__) {...})
+                }
+            ]
+
+            The value of the first index represents the chunk name,
+            which is used to cache the contents of this module in the
+            `installedChunks` object in the main bundle file.
+
+            The second index is an object with a key representing the
+            module's path, and the value being the module itself wrapped
+            in a function expression.  The module's contents are
+            stringified, and passed into an `eval` call.
+
+
+        Evaluated content:
+        Webpack first creates an export object for this module's
+        contents to be applied to.  This is done in __webpack_require__.r,
+        which mutates the __webpack_exports__ object to include
+        properties `Symbol.toStringTag` of value "Module", and
+        `__esModule` of value `true`.
+
+        {
+            Symbol(Symbol.toStringTag): 'Module',
+            __esModule: true
+        }
+
+        Next, __webpack_exports__ is further mutated to include a 'default'
+        property whose value is the file's default export.  This is what's
+        offered when the module's dynamically imported.
+
+        {
+            default: {
+                complete: f,
+                Symbol(Symbol.toStringTag): 'Module',
+                __esModule: true,
+                proto: Object
+            }
+        }
+
+        The end of the stringified contents contains a sourceURL comment
+        which is used for source maps.
+     */
 
     /***/ "./src/todo.js":
     /*!*********************!*\
@@ -13,21 +61,8 @@
     /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
     "use strict";
-    eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\n  complete: el => el.style = 'text-decoration: line-through;'\n});\n\n//# sourceURL=webpack:///./src/todo.js?");
-    /*
-      Code here is executed similarly to the other files. The only major difference is that this one
-      has a default export. This is simply set to a `default` prop on the `__webpack_exports__` object,
-      further mutating it. In the end it'll look something like:
+    eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\n  complete: el => {\n    if (el.getAttribute('disabled') != 'true') {\n      el.setAttribute('disabled', true);\n    }\n  }\n});\n\n//# sourceURL=webpack:///./src/todo.js?");
 
-      {
-        default: {
-          complete: f,
-          Symbol(Symbol.toStringTag): 'Module',
-          __esModule: true,
-          proto: Object
-        }
-      }
-    */
     /***/ })
 
     }]);
