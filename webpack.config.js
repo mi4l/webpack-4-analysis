ccae65bcff8e8494a236921c5c75bbb08869111b
Mike Skalandunas <mskalandunas@gmail.com>
2020-09-01 23:08:06 -0700

    remove progress plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const HTMLWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
 
 module.exports = {
-    mode: 'development',
-    plugins: [new HTMLWebpackPlugin(), new webpack.ProgressPlugin()]
+  mode: 'development',
+  plugins: [new HTMLWebpackPlugin()]
 };
