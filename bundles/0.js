e47c1051a2d19f2b895078c01fc54b4f0a7b224c
Mike Skalandunas <mskalandunas@gmail.com>
2020-09-25 19:01:24 -0700

    remove file name comments

--- bundles/0.js
+++ bundles/0.js
@@ -1,7 +1,7 @@
 (window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],{
   "./src/todo.js": (function(module, __webpack_exports__, __webpack_require__) {
-    // ./src/todo.js
     "use strict";
+    
     eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\n  complete: el => {\n    if (el.getAttribute('disabled') != 'true') {\n      el.setAttribute('disabled', true);\n    }\n  }\n});\n\n//# sourceURL=webpack:///./src/todo.js?");
 
   })
