5365ac8862c03d5565a466d834bd45cc333cb434
Mike Skalandunas <mskalandunas@gmail.com>
2020-09-25 18:53:47 -0700

    remove comments; format

--- bundles/main.js
+++ bundles/main.js
@@ -1,13 +1,9 @@
-(function(modules) { // webpackBootstrap
-  // install a JSONP callback for chunk loading
+(function(modules) {
   function webpackJsonpCallback(data) {
     var chunkIds = data[0];
     var moreModules = data[1];
-
-
-    // add "moreModules" to the modules object,
-    // then flag all "chunkIds" as loaded and fire callback
     var moduleId, chunkId, i = 0, resolves = [];
+
     for(;i < chunkIds.length; i++) {
       chunkId = chunkIds[i];
       if(Object.prototype.hasOwnProperty.call(installedChunks, chunkId) && installedChunks[chunkId]) {
@@ -15,83 +11,63 @@
       }
       installedChunks[chunkId] = 0;
     }
+
     for(moduleId in moreModules) {
       if(Object.prototype.hasOwnProperty.call(moreModules, moduleId)) {
         modules[moduleId] = moreModules[moduleId];
       }
     }
-    if(parentJsonpFunction) parentJsonpFunction(data);
+
+    if(parentJsonpFunction) {
+      parentJsonpFunction(data);
+    };
 
     while(resolves.length) {
       resolves.shift()();
     }
-
   };
 
-
-  // The module cache
   var installedModules = {};
-
-  // object to store loaded and loading chunks
-  // undefined = chunk not loaded, null = chunk preloaded/prefetched
-  // Promise = chunk loading, 0 = chunk loaded
   var installedChunks = {
     "main": 0
   };
 
-
-
-  // script path function
   function jsonpScriptSrc(chunkId) {
     return __webpack_require__.p + "" + ({}[chunkId]||chunkId) + ".js"
   }
 
-  // The require function
   function __webpack_require__(moduleId) {
-
-    // Check if module is in cache
     if(installedModules[moduleId]) {
       return installedModules[moduleId].exports;
     }
-    // Create a new module (and put it into the cache)
+    
     var module = installedModules[moduleId] = {
       i: moduleId,
       l: false,
       exports: {}
     };
 
-    // Execute the module function
-    modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
 
-    // Flag the module as loaded
+    modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
     module.l = true;
 
-    // Return the exports of the module
     return module.exports;
   }
 
-  // This file contains only the entry chunk.
-  // The chunk loading function for additional chunks
   __webpack_require__.e = function requireEnsure(chunkId) {
     var promises = [];
-
-
-    // JSONP chunk loading for javascript
-
     var installedChunkData = installedChunks[chunkId];
-    if(installedChunkData !== 0) { // 0 means "already installed".
 
-      // a Promise means "currently loading".
+    if(installedChunkData !== 0) {
       if(installedChunkData) {
         promises.push(installedChunkData[2]);
       } else {
-        // setup Promise in chunk cache
         var promise = new Promise(function(resolve, reject) {
           installedChunkData = installedChunks[chunkId] = [resolve, reject];
         });
+
         promises.push(installedChunkData[2] = promise);
 
-        // start chunk loading
         var script = document.createElement('script');
         var onScriptComplete;
 
@@ -102,50 +78,53 @@
         }
         script.src = jsonpScriptSrc(chunkId);
 
-        // create error before stack unwound to get useful stacktrace later
         var error = new Error();
+
         onScriptComplete = function (event) {
-          // avoid mem leaks in IE.
           script.onerror = script.onload = null;
+
           clearTimeout(timeout);
+
           var chunk = installedChunks[chunkId];
+
           if(chunk !== 0) {
             if(chunk) {
               var errorType = event && (event.type === 'load' ? 'missing' : event.type);
               var realSrc = event && event.target && event.target.src;
+
               error.message = 'Loading chunk ' + chunkId + ' failed.\n(' + errorType + ': ' + realSrc + ')';
               error.name = 'ChunkLoadError';
               error.type = errorType;
               error.request = realSrc;
+
               chunk[1](error);
             }
+
             installedChunks[chunkId] = undefined;
           }
         };
         var timeout = setTimeout(function(){
           onScriptComplete({ type: 'timeout', target: script });
         }, 120000);
+
         script.onerror = script.onload = onScriptComplete;
+
         document.head.appendChild(script);
       }
     }
+
     return Promise.all(promises);
   };
 
-  // expose the modules object (__webpack_modules__)
   __webpack_require__.m = modules;
-
-  // expose the module cache
   __webpack_require__.c = installedModules;
 
-  // define getter function for harmony exports
   __webpack_require__.d = function(exports, name, getter) {
     if(!__webpack_require__.o(exports, name)) {
       Object.defineProperty(exports, name, { enumerable: true, get: getter });
     }
   };
 
-  // define __esModule on exports
   __webpack_require__.r = function(exports) {
     if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
       Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
@@ -153,7 +132,6 @@
     Object.defineProperty(exports, '__esModule', { value: true });
   };
 
-  // create a fake namespace object
   // mode & 1: value is a module id, require it
   // mode & 2: merge all properties of value into the ns
   // mode & 4: return value when already ns object
@@ -169,7 +147,6 @@
     return ns;
   };
 
-  // getDefaultExport function for compatibility with non-harmony modules
   __webpack_require__.n = function(module) {
     var getter = module && module.__esModule ?
       function getDefault() { return module['default']; } :
@@ -178,42 +155,34 @@
     return getter;
   };
 
-  // Object.prototype.hasOwnProperty.call
-  __webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+  __webpack_require__.o = function(object, property) {
+    return Object.prototype.hasOwnProperty.call(object, property); 
+  };
 
-  // __webpack_public_path__
   __webpack_require__.p = "";
-
-  // on error function for async loading
   __webpack_require__.oe = function(err) { console.error(err); throw err; };
 
   var jsonpArray = window["webpackJsonp"] = window["webpackJsonp"] || [];
   var oldJsonpFunction = jsonpArray.push.bind(jsonpArray);
+
   jsonpArray.push = webpackJsonpCallback;
   jsonpArray = jsonpArray.slice();
-  for(var i = 0; i < jsonpArray.length; i++) webpackJsonpCallback(jsonpArray[i]);
+
+  for(var i = 0; i < jsonpArray.length; i++) {
+    webpackJsonpCallback(jsonpArray[i])
+  }
+
   var parentJsonpFunction = oldJsonpFunction;
 
-
-  // Load entry module and return exports
   return __webpack_require__(__webpack_require__.s = "./src/index.js");
-})
-/************************************************************************/
-({
+})({
   "./src/index.js": (function(module, __webpack_exports__, __webpack_require__) {
-  /*!**********************!*\
-    !*** ./src/index.js ***!
-    \**********************/
-  /*! no exports provided */
+    // ./src/index.js
     "use strict";
     eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _renderUtils__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./renderUtils */ \"./src/renderUtils.js\");\n\n\nconst getTodoHandlers = () => __webpack_require__.e(/*! import() */ 0).then(__webpack_require__.bind(null, /*! ./todo */ \"./src/todo.js\"));\n\n(todos => {\n  const list = document.createElement('ul');\n\n  list.innerHTML = todos.reduce(\n    (html, todo) => html + Object(_renderUtils__WEBPACK_IMPORTED_MODULE_0__[\"listItemTemplate\"])(todo),\n    ''\n  );\n  list.addEventListener('click', e => {\n    if (e.target.tagName === 'BUTTON') {\n      getTodoHandlers().then(m => {\n        m.default.complete(e.target);\n      });\n    }\n  });\n\n  document\n    .getElementsByTagName('body')[0]\n    .appendChild(list);\n})([\n  'Repot plants',\n  'Fill bird feeders',\n  'Restring guitar'\n]);\n\n//# sourceURL=webpack:///./src/index.js?");
-
   }),
   "./src/renderUtils.js": (function(module, __webpack_exports__, __webpack_require__) {
-  /*!****************************!*\
-    !*** ./src/renderUtils.js ***!
-    \****************************/
-  /*! exports provided: listItemTemplate */
+    // ./src/renderUtils.js
     "use strict";
     eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"listItemTemplate\", function() { return listItemTemplate; });\nconst listItemTemplate = text => `<li><button>${text}</button></li>`;\n\n//# sourceURL=webpack:///./src/renderUtils.js?");
   })
